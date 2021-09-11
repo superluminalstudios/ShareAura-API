5ac24974f35b1c532d6d6ab1a6a2ba7374aa1388
Lona.exe <39522664+JeswinSunsi@users.noreply.github.com>
2021-09-11 15:45:05 +0400

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,88 +1,47 @@
+from replit import db
 from flask import Flask, request, jsonify
-import pymongo, os
-from dotenv import load_dotenv
-import dns
-from datetime import datetime
-
-# Connect to DB
-load_dotenv()
-MONGODB_URI = os.environ.get("MONGODB_URI")
-client = pymongo.MongoClient(MONGODB_URI)
-db = client.database
-collection = db.requests
 
 
-# Init webserver
 app = Flask(__name__)
-app.config['JSON_SORT_KEYS'] = False
+
+@app.route("/signup/<phone>/<password>", methods=["GET"])
+def signUp(phone=0,password=0):
+  phone, password = str(phone), str(password)
+  try:
+    db[phone] = password
+    response = jsonify({"proceed":"True"})
+  except:
+    response = jsonify({"proceed":"False"})
+  response.headers.add("Access-Control-Allow-Origin", "*")
+  response.headers.add("Access-Control-Allow-Headers", "*")
+  response.headers.add("Access-Control-Allow-Methods", "*")
+  return response
 
 
-# Categories
-@app.route("/category/<cat>/<quantity>", methods=["GET"])
-def mainPageCats(cat="textbook", quantity="3"):
-    query = {"itemtype": {"$in": [cat]}}
-    result = collection.find(query).limit(int(quantity)).sort("_id", -1)
-    data = {}
-    for item in result:
-      item['_id'] = str(item['_id']) 
-      data[item['_id']] = item
-      item['itemList'] = item['items'].split("+")
-      item['items'] = str(item['items']).replace("+", " · ")    
-    response = jsonify(data)
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Headers", "*")
-    response.headers.add("Access-Control-Allow-Methods", "*")
-    return response
-    
-# Search
-@app.route("/search/<item>", methods=["GET"])
-def searchRequests(item="Phy 11"):
-    item = item.title()
-    query = {"items": {"$regex" : item}}
-    result = collection.find(query).limit(15).sort("_id", -1)
-    data = {}
-    for item in result:
-      item['_id'] = str(item['_id']) 
-      data[item['_id']] = item
-      item['itemList'] = item['items'].split("+")
-      item['items'] = str(item['items']).replace("+", " · ") 
-      
-    response = jsonify(data)
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Headers", "*")
-    response.headers.add("Access-Control-Allow-Methods", "*")
-    return response
 
 
-# Add Req
-@app.route("/add/<username>/<usernumber>/<items>", methods=["GET"])
-def addRequest(username="", usernumber="", items=""):
-    items.split("+")
-
-    firstItem=items.split("+")[0]
-    textbooks=['Maths 11', 'Chem 11', 'Phy 11', 'Bio 11', 'Applied Maths 11']
-
-    if firstItem in textbooks:
-      itemtype="textbook"
-    elif 'CW' in firstItem:
-      itemtype = "notebook"
+@app.route("/signin/<phone>/<password>", methods=["GET"])
+def signIn(phone=0,password=0):
+  phone, password = str(phone), str(password)
+  try:
+    match = db.prefix(phone)
+    match = str(match[0])
+    value=db[match]
+    stored_password = value.split('-break-', 1)[1]
+    full_name = value.replace(stored_password, '')
+    full_name = full_name.replace('-break-', '')
+    if stored_password == password:
+      response = jsonify({"proceed" : "True", "name": full_name})
     else:
-      itemtype = "stationary"
-
-    data = {
-        "username":  username,
-        "usernumber": usernumber,
-        "itemtype": itemtype,
-        "items": items,
-        "date": datetime.now()
-    }
-    collection.insert_one(data)
-    response = jsonify("data")
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Headers", "*")
-    response.headers.add("Access-Control-Allow-Methods", "*")
-    return response
-
+      response = jsonify({"proceed":"False"})
+  except:
+    response = jsonify("False")
+  response.headers.add("Access-Control-Allow-Origin", "*")
+  response.headers.add("Access-Control-Allow-Headers", "*")
+  response.headers.add("Access-Control-Allow-Methods", "*")
+  return response
+  
+  
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
